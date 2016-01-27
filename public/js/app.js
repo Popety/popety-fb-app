05083e4843d5557d95c36b22214c7778a487754a
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-01-27 12:32:31 +0530

    fblogin in process

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,29 @@ angular.module('popetyfbapp',['ui.router', 'MassAutoComplete','ngMessages','jkur
       };
     }
   }; 
-})    
+}) 
+
+.run( function( $rootScope ) {
+  // Load the facebook SDK asynchronously
+  (function(){
+     // If we've already installed the SDK, we're done
+     if (document.getElementById('facebook-jssdk')) {return;}
+
+     // Get the first script element, which we'll use to find the parent node
+     var firstScriptElement = document.getElementsByTagName('script')[0];
+
+     // Create a new script element and set its id
+     var facebookJS = document.createElement('script'); 
+     facebookJS.id = 'facebook-jssdk';
+
+     // Set the new script's source to the source of the Facebook JS SDK
+     facebookJS.src = '//connect.facebook.net/en_US/all.js';
+
+     // Insert the Facebook JS SDK into the DOM
+     firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+   }());
+})
+
 
 .config(function($stateProvider, $urlRouterProvider,$facebookProvider) {
 
@@ -78,4 +100,5 @@ angular
     .controller('AppController', function($scope, FileUploader) {
         $scope.uploader = new FileUploader();
     });
+    
 
