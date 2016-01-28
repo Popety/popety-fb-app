8a6adb109ecd60113ff6524f3a348b42f428200a
ftechiesnitin <nitin.dolli@fountaintechies.com>
2016-01-28 15:42:40 +0530

    configs changed

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,8 @@
 var instance = "local";
 
-if (instance == "dev"){
+if (instance == "prod"){
+	var baseurl = 'http://apps.popety.com:4444/api/';
+}else if (instance == "dev"){
 	var baseurl = 'http://n2.transparent.sg:9999/api/';
 }else if(instance == "local"){
 	var baseurl = 'http://localhost:9999/api/';
