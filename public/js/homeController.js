12890a106048507c0fba1ad38b1753d4b2a8d7fc
ftechiesnitin <nitin.dolli@fountaintechies.com>
2016-01-29 11:54:04 +0530

    facebook login under development

--- public/js/homeController.js
+++ public/js/homeController.js
@@ -1,23 +1,31 @@
 angular.module('popetyfbapp')
 
 .controller('homeController', function ($scope, $http, ezfb) {
+
   ezfb.login(function (res) {
     console.log('5', res);
      /**
       * no manual $scope.$apply, I got that handled
       */
      if (res.authResponse) {
-       updateLoginStatus(updateApiMe);
+       updateLoginStatus(authResponse);
      }
    }, {scope: 'email,user_likes'});
+
   /**
    * Update loginStatus result
    */
   function updateLoginStatus (more) {
-    ezfb.getLoginStatus(function (res) {
-      $scope.loginStatus = res;
-      console.log('19', res);
-      (more || angular.noop)();
+    $http.get("https://graph.facebook.com/v2.5/me", {
+        params: {
+          access_token: more.accessToken,
+          fields: "id,name,email,gender,first_name,last_name,location,website,picture,relationship_status",
+          format: "json"
+        }
+    }).then(function(result) {
+      console.log('26', result);
+    }, function(error) {
+        console.log("There was a problem getting your profile.  Check the logs for details.");
     });
   }
 
