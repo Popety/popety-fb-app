5d2e66625ef63e04c61761693a4fadbb797957a5
ftechiesnitin <nitin.dolli@fountaintechies.com>
2016-01-29 18:04:38 +0530

    likes counting

--- public/js/homeController.js
+++ public/js/homeController.js
@@ -2,24 +2,25 @@ angular.module('popetyfbapp')
 
 .controller('homeController', function ($scope, $http, ezfb) {
 
-  // ezfb.login(function (res) {
-  //    if(res.status === 'connected'){
-  //      if (res.authResponse) {
-  //        updateLoginStatus(res.authResponse);
-  //      }
-  //    }else {
-  //      console.log('login');
-  //    }
-  //  }, {scope: 'email'});
+  $scope.login = function () {
+    ezfb.login(function (res) {
+       if(res.status === 'connected'){
+         if (res.authResponse) {
+           updateLoginStatus(res.authResponse);
+         }
+       }else {
+         console.log('login');
+       }
+     }, {scope: 'email, user_likes'});
+  };
 
   /**
    * Update loginStatus result
    */
   function updateLoginStatus (more) {
-    $http.get("https://graph.facebook.com/v2.5/me", {
+    $http.get("https://graph.facebook.com/v2.5/me/likes", {
         params: {
           access_token: more.accessToken,
-          fields: "id,name,email,gender,first_name,last_name,location,website,picture,relationship_status",
           format: "json"
         }
     }).then(function(result) {
@@ -27,15 +28,17 @@ angular.module('popetyfbapp')
     }, function(error) {
         console.log("There was a problem getting your profile.  Check the logs for details.");
     });
+    // $http.get("https://graph.facebook.com/v2.5/me", {
+    //     params: {
+    //       access_token: more.accessToken,
+    //       fields: "id,name,email,gender,first_name,last_name,location,website,picture,relationship_status",
+    //       format: "json"
+    //     }
+    // }).then(function(result) {
+    //   console.log('26', result);
+    // }, function(error) {
+    //     console.log("There was a problem getting your profile.  Check the logs for details.");
+    // });
   }
 
-  /**
-   * Update api('/me') result
-   */
-  function updateApiMe () {
-    ezfb.api('/me', function (res) {
-      $scope.apiMe = res;
-      console.log('30',res);
-    });
-  }
 });
