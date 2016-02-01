26967e6cc3433ed1aec035320599d59c5f98d620
ftechiesnitin <nitin.dolli@fountaintechies.com>
2016-02-01 09:01:39 +0530

    function under development: testing 5

--- public/js/homeController.js
+++ public/js/homeController.js
@@ -2,7 +2,7 @@ angular.module('popetyfbapp')
 
 .controller('homeController', function ($scope, $http, ezfb, $q) {
 
-  $scope.like_1 = function () {
+  $scope.fb_submit = function () {
     console.log('hello');
     ezfb.login(function (res) {
        if(res.status === 'connected'){
